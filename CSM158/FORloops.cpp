0975a981be5beb74750886100984171ad04210b9
melo1dp <laaditabaka@gmail.com>
2024-06-11 02:59:13 -0700

    Assignment

--- CSM158/FORloops.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-using namespace std;
-
-int main(){
-   string myCars[] = {"Volvo", "Mercedes", "CRV", "Accord"};
-        for (int i = 0; i <= sizeof(myCars)/sizeof(string); i++)
-        cout << i << endl;
-return 0;
-}
